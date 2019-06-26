f8d03e288c725a7652fa7ed9cde9830d2b825853
Paul J Kane <paulkane2204@hotmail.com>
2019-06-26 18:30:36 +0100

    Added PatientManager module

---
+++ PJK.PRISM.HCIS2.Module.PatientManager/Views/RibbonMain.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace PJK.PRISM.HCIS2.Module.PatientManager.Views
+{
+    /// <summary>
+    /// Interaction logic for RibbonMain
+    /// </summary>
+    public partial class RibbonMain : UserControl
+    {
+        public RibbonMain()
+        {
+            InitializeComponent();
+        }
+    }
+}
